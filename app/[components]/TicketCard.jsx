ad5b2d2b2d2d0933004d3cb6e7ced4aae5843e66
Akshay Rathod <kr.akshay234@gmail.com>
2023-11-14 20:49:07 +0530

    updated ticket card

--- app/[components]/TicketCard.jsx
+++ app/[components]/TicketCard.jsx
@@ -1,11 +1,23 @@
-import DeleteBlock from "./DeleteBlock"
+import DeleteBlock from "./DeleteBlock";
 
 const TicketCard = () => {
   return (
-    <div className="">
-      <DeleteBlock />
+    <div className="flex flex-col w-[250px] h-[250px] space-y-4">
+      <div className="place-self-end">
+        <DeleteBlock />
+      </div>
+      <div className="flex flex-col px-4">
+        <p className="text-default-text">Title</p>
+        <p className="text-default-text">Description</p>
+        <p className="text-default-text">Status</p>
+        <p className="text-default-text">Priority</p>
+      </div>
+      <div className="flex flex-col px-4">
+        <p className="text-default-text">Created By</p>
+        <p className="text-default-text">Assigned To</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TicketCard
+export default TicketCard;
