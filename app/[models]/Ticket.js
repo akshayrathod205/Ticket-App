3af11c7221c70182af7c626f43c888917aa1f1d4
Akshay Rathod <kr.akshay234@gmail.com>
2023-12-08 22:30:22 +0530

    updated schema

--- app/[models]/Ticket.js
+++ app/[models]/Ticket.js
@@ -3,44 +3,45 @@ import mongoose, { Schema } from "mongoose";
 mongoose.connect(process.env.MONGO_URI);
 mongoose.Promise = global.Promise;
 
-const TicketSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const TicketSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    deadline: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    priority: {
+      type: String,
+      required: true,
+    },
+    assignedTo: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+    },
+    updatedAt: {
+      type: Date,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  deadline: {
-    type: Date,
-    required: true,
-  },
-  status: {
-    type: String,
-    required: true,
-  },
-  priority: {
-    type: String,
-    required: true,
-  },
-  assignedTo: {
-    type: String,
-    required: true,
-  },
-  createdBy: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    required: true,
-  },
-  updatedAt: {
-    type: Date,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Ticket = mongoose.models.Ticket || mongoose.model("Ticket", TicketSchema);
 export default Ticket;
