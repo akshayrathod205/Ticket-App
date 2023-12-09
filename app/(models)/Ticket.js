cdca512276eb498723d4af3f4286e1e901c9daa5
Akshay Rathod <kr.akshay234@gmail.com>
2023-12-09 13:48:04 +0530

    updated model

---
+++ app/(models)/Ticket.js
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+
+mongoose.connect(process.env.MONGO_URI);
+mongoose.Promise = global.Promise;
+
+const TicketSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    deadline: {
+      type: Date,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    priority: {
+      type: String,
+      required: true,
+    },
+    assignedTo: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+    },
+    updatedAt: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+const Ticket = mongoose.models.Ticket || mongoose.model("Ticket", TicketSchema);
+export default Ticket;
