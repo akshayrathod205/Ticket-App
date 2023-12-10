c122edfb8ca699f0881359d8ee42ec2296849e39
Akshay Rathod <kr.akshay234@gmail.com>
2023-12-10 23:35:26 +0530

    added delete function

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -2,11 +2,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
-const DeleteBlock = () => {
+const DeleteBlock = ({ id }) => {
+  const handleDelete = async () => {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+      method: "DELETE",
+    });
+    if (res.status === 200) {
+      window.location.reload();
+    }
+  };
+
   return (
     <FontAwesomeIcon
       icon={faTrash}
       className="text-default-text hover:cursor-pointer"
+      onClick={handleDelete}
     />
   );
 };
