06833c387eba2072c3d6a0b9400c35e94d29e3d2
Akshay Rathod <kr.akshay234@gmail.com>
2023-12-10 23:36:14 +0530

    get, put, delete done

---
+++ app/api/Tickets/[id]/route.js
@@ -0,0 +1,36 @@
+import Ticket from "@/app/(models)/Ticket";
+import { NextResponse } from "next/server";
+
+export async function GET(req, { params }) {
+  try {
+    const { id } = params;
+    const ticket = await Ticket.findOne({ _id: id });
+    return NextResponse.json(ticket, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
+
+export async function PUT(req, { params }) {
+  try {
+    const { id } = params;
+    const body = await req.json();
+    const ticketData = body.formData;
+    const updatedTicket = await Ticket.findByIdAndUpdate(id, ticketData, {
+      new: true,
+    });
+    return NextResponse.json(updatedTicket, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
+
+export async function DELETE(req, { params }) {
+  try {
+    const { id } = params;
+    await Ticket.findByIdAndDelete(id);
+    return NextResponse.json({ message: "Ticket deleted" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
