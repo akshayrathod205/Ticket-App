a21a5dd4344f7db333e082c16e88fe92c46258d7
Akshay Rathod <kr.akshay234@gmail.com>
2023-12-10 23:35:57 +0530

    updated

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -4,11 +4,12 @@ import { useState } from "react";
 
 const TicketCard = (props) => {
   const [ticket, setTicket] = useState(props.ticket);
+
   return (
     <div className="flex flex-col w-[400px] h-auto space-y-4 p-5 border rounded-md shadow-md">
       <div className="flex justify-between px-4">
         <Field label={ticket.title} />
-        <DeleteBlock />
+        <DeleteBlock id={ticket._id} />
       </div>
       <div className="flex flex-col px-4 py-2">
         <div className="grid grid-cols-2">
