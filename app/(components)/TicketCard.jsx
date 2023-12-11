662667ccf7a5af7b9570582642bfd078bfa4c73a
Akshay Rathod <kr.akshay234@gmail.com>
2023-12-11 19:47:48 +0530

    added routes

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -1,14 +1,22 @@
 import DeleteBlock from "./DeleteBlock";
 import Field from "./Field";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const TicketCard = (props) => {
+  const router = useRouter();
   const [ticket, setTicket] = useState(props.ticket);
 
+  const handleClick = () => {
+    router.push(`/ticket/${ticket._id}`);
+  };
+  
   return (
     <div className="flex flex-col w-[400px] h-auto space-y-4 p-5 border rounded-md shadow-md">
       <div className="flex justify-between px-4">
-        <Field label={ticket.title} />
+        <div className="cursor-pointer" onClick={handleClick}>
+          <Field label={ticket.title} />
+        </div>
         <DeleteBlock id={ticket._id} />
       </div>
       <div className="flex flex-col px-4 py-2">
