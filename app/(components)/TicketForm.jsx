f0b7bbaf20a0cbb47836b029e514d09199ca119f
Akshay Rathod <kr.akshay234@gmail.com>
2023-12-12 23:48:40 +0530

    edit ticket done

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -1,9 +1,168 @@
+// "use client";
+// import { useRouter } from "next/navigation";
+// import { useState } from "react";
+
+// const TicketForm = ({ ticket }) => {
+//   const editMode = ticket.id === "new" ? false : true;
+
+//   const router = useRouter();
+//   const [formData, setFormData] = useState({
+//     title: "",
+//     description: "",
+//     deadline: "",
+//     priority: "Low",
+//     status: "Not Started",
+//     assignedTo: "",
+//     createdBy: "Akshay",
+//   });
+
+//   if (editMode) {
+//     setFormData({
+//       title: ticket.title,
+//       description: ticket.description,
+//       deadline: ticket.deadline,
+//       priority: ticket.priority,
+//       status: ticket.status,
+//       assignedTo: ticket.assignedTo,
+//       createdBy: ticket.createdBy,
+//     });
+
+//   }
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     const updatedValue =
+//       name === "deadline" ? new Date(value).toISOString().split("T")[0] : value;
+//     setFormData((prevData) => ({
+//       ...prevData,
+//       [name]: updatedValue,
+//     }));
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     console.log(formData);
+//     const res = await fetch("/api/Tickets", {
+//       method: "POST",
+//       body: JSON.stringify({ formData }),
+//       "Content-type": "application/json",
+//     });
+//     console.log(res);
+//     if (res.ok) {
+//       router.refresh();
+//       router.push("/");
+//     } else {
+//       console.error("Error");
+//     }
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <div className="flex flex-col space-y-4">
+//         <h3 className="self-center text-2xl font-semibold">Create</h3>
+//         <div className="flex flex-col space-y-2">
+//           <label htmlFor="title">Title</label>
+//           <input
+//             type="text"
+//             name="title"
+//             id="title"
+//             className="border border-gray-400 rounded-md p-2"
+//             value={formData.title}
+//             onChange={handleChange}
+//           />
+//         </div>
+//         <div className="flex flex-col space-y-2">
+//           <label htmlFor="description">Description</label>
+//           <textarea
+//             name="description"
+//             id="description"
+//             cols="30"
+//             rows="10"
+//             className="border border-gray-400 rounded-md p-2"
+//             value={formData.description}
+//             onChange={handleChange}
+//           ></textarea>
+//         </div>
+//         <div className="flex flex-col space-y-2">
+//           <label htmlFor="deadline">Deadline</label>
+//           <input
+//             type="date"
+//             name="deadline"
+//             id="deadline"
+//             className="border border-gray-400 rounded-md p-2"
+//             value={formData.deadline}
+//             onChange={handleChange}
+//           />
+//         </div>
+//         {/* <div className="flex flex-col space-y-2">
+//           <label htmlFor="status">Status</label>
+//           <select
+//             name="status"
+//             id="status"
+//             className="border border-gray-400 rounded-md p-2"
+//             value={formData.status}
+//             onChange={handleChange}
+//           >
+//             <option value="Not Started">Not Started</option>
+//             <option value="In Progress">In Progress</option>
+//             <option value="Completed">Completed</option>
+//           </select>
+//         </div> */}
+//         <div className="flex flex-col space-y-2">
+//           <label htmlFor="priority">Priority</label>
+//           <select
+//             name="priority"
+//             id="priority"
+//             className="border border-gray-400 rounded-md p-2"
+//             value={formData.priority}
+//             onChange={handleChange}
+//           >
+//             <option value="Low">Low</option>
+//             <option value="Medium">Medium</option>
+//             <option value="High">High</option>
+//           </select>
+//         </div>
+//         {/* <div className="flex flex-col space-y-2">
+//           <label htmlFor="created-by">Created By</label>
+//           <input
+//             type="text"
+//             name="created-by"
+//             id="created-by"
+//             className="border border-gray-400 rounded-md p-2"
+//           />
+//         </div> */}
+//         <div className="flex flex-col space-y-2">
+//           <label htmlFor="assigned-to">Assigned To</label>
+//           <input
+//             type="text"
+//             name="assignedTo"
+//             id="assignedTo"
+//             className="border border-gray-400 rounded-md p-2"
+//             value={formData.assignedTo}
+//             onChange={handleChange}
+//           />
+//         </div>
+//         <div className="flex flex-col space-y-2">
+//           <button
+//             type="submit"
+//             className="bg-default-text text-white rounded-md p-2"
+//           >
+//             Create
+//           </button>
+//         </div>
+//       </div>
+//     </form>
+//   );
+// };
+
+// export default TicketForm;
+
 "use client";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const TicketForm = ({ ticket }) => {
-  const editMode = ticket._id === "new" ? false : true;
+  const editMode = ticket.id !== "new";
 
   const router = useRouter();
   const [formData, setFormData] = useState({
@@ -16,9 +175,19 @@ const TicketForm = ({ ticket }) => {
     createdBy: "Akshay",
   });
 
-  if (editMode) {
-    console.log(ticket);
-  }
+  useEffect(() => {
+    if (editMode) {
+      setFormData({
+        title: ticket.title || "",
+        description: ticket.description || "",
+        deadline: ticket.deadline || "",
+        priority: ticket.priority || "Low",
+        status: ticket.status || "Not Started",
+        assignedTo: ticket.assignedTo || "",
+        createdBy: ticket.createdBy || "Akshay",
+      });
+    }
+  }, [editMode, ticket]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -36,7 +205,9 @@ const TicketForm = ({ ticket }) => {
     const res = await fetch("/api/Tickets", {
       method: "POST",
       body: JSON.stringify({ formData }),
-      "Content-type": "application/json",
+      headers: {
+        "Content-type": "application/json",
+      },
     });
     console.log(res);
     if (res.ok) {
@@ -50,7 +221,9 @@ const TicketForm = ({ ticket }) => {
   return (
     <form onSubmit={handleSubmit}>
       <div className="flex flex-col space-y-4">
-        <h3 className="self-center text-2xl font-semibold">Create</h3>
+        <h3 className="self-center text-2xl font-semibold">
+          {editMode ? "Edit" : "Create"}
+        </h3>
         <div className="flex flex-col space-y-2">
           <label htmlFor="title">Title</label>
           <input
@@ -86,19 +259,19 @@ const TicketForm = ({ ticket }) => {
           />
         </div>
         {/* <div className="flex flex-col space-y-2">
-          <label htmlFor="status">Status</label>
-          <select
-            name="status"
-            id="status"
-            className="border border-gray-400 rounded-md p-2"
-            value={formData.status}
-            onChange={handleChange}
-          >
-            <option value="Not Started">Not Started</option>
-            <option value="In Progress">In Progress</option>
-            <option value="Completed">Completed</option>
-          </select>
-        </div> */}
+           <label htmlFor="status">Status</label>
+           <select
+             name="status"
+             id="status"
+             className="border border-gray-400 rounded-md p-2"
+             value={formData.status}
+             onChange={handleChange}
+           >
+             <option value="Not Started">Not Started</option>
+             <option value="In Progress">In Progress</option>
+             <option value="Completed">Completed</option>
+           </select>
+         </div> */}
         <div className="flex flex-col space-y-2">
           <label htmlFor="priority">Priority</label>
           <select
@@ -114,14 +287,14 @@ const TicketForm = ({ ticket }) => {
           </select>
         </div>
         {/* <div className="flex flex-col space-y-2">
-          <label htmlFor="created-by">Created By</label>
-          <input
-            type="text"
-            name="created-by"
-            id="created-by"
-            className="border border-gray-400 rounded-md p-2"
-          />
-        </div> */}
+           <label htmlFor="created-by">Created By</label>
+           <input
+             type="text"
+             name="created-by"
+             id="created-by"
+             className="border border-gray-400 rounded-md p-2"
+           />
+         </div> */}
         <div className="flex flex-col space-y-2">
           <label htmlFor="assigned-to">Assigned To</label>
           <input
@@ -138,7 +311,7 @@ const TicketForm = ({ ticket }) => {
             type="submit"
             className="bg-default-text text-white rounded-md p-2"
           >
-            Create
+            {editMode ? "Update" : "Create"}
           </button>
         </div>
       </div>
--- app/ticket/[id]/page.jsx
+++ app/ticket/[id]/page.jsx
@@ -1,35 +1,95 @@
-"use client"; 
+// "use client";
+// import TicketForm from "@/app/(components)/TicketForm";
+// import { useRouter } from "next/navigation";
+// import { useEffect } from "react";
+
+// const getTicket = async (id) => {
+//   const res = await fetch(`http://localhost:3000/api/Tickets/${id}`);
+
+//   if (res.status === 200) {
+//     return res.json();
+//   } else {
+//     console.error("Error");
+//   }
+// };
+
+// const Ticket = ({ params }) => {
+//   const router = useRouter();
+//   const editMode = params.id === "new" ? false : true;
+//   let updateTicketData = {};
+
+//   const fetchTicket = async () => {
+//     if (editMode) {
+//       updateTicketData = await getTicket(params.id);
+//       updateTicketData = updateTicketData.ticket;
+//     } else {
+//       updateTicketData = {
+//         id: "new",
+//       };
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchTicket();
+//   }, [params.id, editMode]);
+
+//   return (
+//     <div className="flex justify-center">
+//       <div className="w-[650px] py-10">
+//         <TicketForm ticket={updateTicketData} />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Ticket;
+
+"use client";
 import TicketForm from "@/app/(components)/TicketForm";
 import { useRouter } from "next/navigation";
-
-const getTicket = async (id) => {
-  const res = await fetch(`http://localhost:3000/api/Tickets/${id}`);
-
-  if (res.status === 200) {
-    return res.json();
-  } else {
-    console.error("Error");
-  }
-};
+import { useEffect, useState } from "react";
 
 const Ticket = ({ params }) => {
   const router = useRouter();
-  const editMode = params.id === "new" ? false : true;
-  let updateTicketData = {};
 
-  const fetchTicket = async () => {
+  const [updateTicketData, setUpdateTicketData] = useState({});
+
+  // const fetchTicket = async () => {
+  //   let ticketData = {};
+  //   if (editMode) {
+  //     ticketData = await getTicket(params.id);
+  //     ticketData = ticketData.ticket;
+  //   } else {
+  //     ticketData = {
+  //       id: "new",
+  //     };
+  //   }
+  //   setUpdateTicketData(ticketData);
+  // };
+
+  useEffect(() => {
+    const editMode = params.id === "new" ? false : true;
+    let ticketData = {};
+    console.log(params.id);
     if (editMode) {
-      updateTicketData = await getTicket(params.id);
-      updateTicketData = updateTicketData.ticket;
+      fetch(`http://localhost:3000/api/Tickets/${params.id}`).then((res) => {
+        if (res.status === 200) {
+          res.json().then((data) => {
+            ticketData = data.ticket;
+            console.log(ticketData);
+            setUpdateTicketData(ticketData);
+          });
+        } else {
+          console.error("Error");
+        }
+      });
     } else {
-      updateTicketData = {
-        _id: "new",
+      ticketData = {
+        id: "new",
       };
     }
-    router.refresh();
-  };
-
-  fetchTicket();
+    setUpdateTicketData(ticketData);
+  }, [params.id]);
 
   return (
     <div className="flex justify-center">
